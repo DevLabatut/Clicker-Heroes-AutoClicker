ff1e9e5437e367af35ae17e4840b91e6848976af
Lucas Labatut <134607946+DevLabatut@users.noreply.github.com>
2024-01-19 08:18:14 -0300

    clicker_autoclicker.py

--- clicker_autoclicker.py
+++ clicker_autoclicker.py
@@ -1,16 +1,12 @@
 import pyautogui
 import time
 
-# Tempo de espera antes do início da automação (em segundos)
 time.sleep(5)
 
 try:
     while True:
-        # Simula um clique do botão esquerdo do mouse
         pyautogui.click()
-        # Aguarda um intervalo muito curto antes do próximo clique (em segundos)
         time.sleep(0.01)
 
 except KeyboardInterrupt:
-    # Captura a exceção KeyboardInterrupt (Ctrl+C) para encerrar a automação
     print("Automação interrompida pelo usuário.")
